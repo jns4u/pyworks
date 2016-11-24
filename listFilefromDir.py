260b9e4216ea38d8d8041dee234b08e25406d497
Jai Narayan Singh <jainarayan1023@gmail.com>
2016-11-25 00:27:52 +0530

    Updated file
    added function to list specific directory contents

--- listFilefromDir.py
+++ listFilefromDir.py
@@ -1,5 +1,16 @@
-#program considers current directory to list contents
 import os
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for f in files:
-    print f
+
+def listCurDirfile():
+    #function considers current directory to list contents
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        print f
+    
+def list_files(path):
+    # returns a list of names (with extension, without full path) of all files in folder path    
+    for name in os.listdir(path):
+        if os.path.isfile(os.path.join(path, name)):
+            print name    
+
+listCurDirfile()
+list_files("enter_dir_name_here")
